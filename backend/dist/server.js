99500d028f7f4e9282c3e4b674810c04cd625bd8
kakimaru <memimei0306@gmail.com>
2024-12-05 09:08:56 +0900

    I added frontend part

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -10,6 +10,7 @@ const body_parser_1 = __importDefault(require("body-parser"));
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const db_1 = __importDefault(require("./config/db"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
+const survey_routes_1 = __importDefault(require("./routes/survey.routes"));
 dotenv_1.default.config();
 // Database
 (0, db_1.default)();
@@ -26,6 +27,7 @@ app.use(body_parser_1.default.urlencoded({ extended: true }));
 app.use((0, cookie_parser_1.default)());
 // Routes
 app.use('/api/users', user_routes_1.default);
+app.use('/api/surveys', survey_routes_1.default);
 // Port
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
