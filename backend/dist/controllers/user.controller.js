392a458614725534deb17d02b646b4427c673e89
kakimaru <memimei0306@gmail.com>
2024-12-06 11:30:21 +0900

    Modified header

--- backend/dist/controllers/user.controller.js
+++ backend/dist/controllers/user.controller.js
@@ -14,71 +14,90 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const user_model_1 = __importDefault(require("../model/user.model"));
 const jsonwebtoken_1 = __importDefault(require("jsonwebtoken"));
-class UserController {
-    //register
-    register(req, res) {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const { username, email, password } = req.body;
-                const user = yield user_model_1.default.create({ email, password, username });
-                const token = jsonwebtoken_1.default.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                res.cookie('jwt', token, { httpOnly: true, sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                    secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 });
-                res.status(201).json({ message: 'User registered successfully' });
-            }
-            catch (error) {
-                res.status(500).json({ message: 'Error registering user' });
-            }
-        });
-    }
-    // login
-    login(req, res) {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const { email, password } = req.body;
-                const user = yield user_model_1.default.findOne({ email });
-                if (!user) {
-                    return res.status(401).json({ message: 'Invalid credentials' });
-                }
-                const isMatch = yield user.matchPassword(password);
-                if (!isMatch) {
-                    return res.status(401).json({ message: 'Invalid credentials' });
-                }
-                const token = jsonwebtoken_1.default.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-                res.cookie('jwt', token, { httpOnly: true, sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-                    secure: process.env.NODE_ENV === 'production', maxAge: 24 * 60 * 60 * 1000 });
-                return res.status(200).json({ message: 'Login successful' });
-            }
-            catch (error) {
-                return res.status(500).json({ message: 'Error logging in', error });
-            }
-        });
-    }
-    // user profile
-    userProfile(req, res) {
-        return __awaiter(this, void 0, void 0, function* () {
-            try {
-                const { username } = req.user;
-                const user = yield user_model_1.default.findOne({ username });
-                if (!user) {
-                    return res.status(404).json({ message: 'User not found' });
-                }
-                return res.status(200).json(user);
-            }
-            catch (error) {
-                return res.status(500).json({ message: 'Error getting user', error });
-            }
-        });
-    }
-    // logout
-    logout(req, res) {
-        res.clearCookie('jwt', {
+//register
+const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username, email, password } = req.body;
+        const user = yield user_model_1.default.create({ email, password, username });
+        const token = jsonwebtoken_1.default.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.cookie("jwt", token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production', // Asegúrate de que esté en true en producción
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
-            maxAge: 0 // Establecer maxAge en 0 asegura que la cookie se elimine inmediatamente
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 24 * 60 * 60 * 1000,
         });
-        res.status(200).json({ message: 'Logout successful' });
+        res.status(201).json({ message: "User registered successfully" });
     }
-}
-exports.default = UserController;
+    catch (error) {
+        res.status(500).json({ message: "Error registering user" });
+    }
+});
+// login
+const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { email, password } = req.body;
+        const user = yield user_model_1.default.findOne({ email });
+        if (!user) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return;
+        }
+        const isMatch = yield user.matchPassword(password);
+        if (!isMatch) {
+            res.status(401).json({ message: "Invalid credentials" });
+            return;
+        }
+        const token = jsonwebtoken_1.default.sign({ id: user._id, username: user.username }, process.env.JWT_SECRET, { expiresIn: "1h" });
+        res.cookie("jwt", token, {
+            httpOnly: true,
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 24 * 60 * 60 * 1000,
+        });
+        res.status(200).json({ message: "Login successful" });
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error logging in", error });
+        return;
+    }
+});
+// user profile
+const userProfile = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { username } = req.user;
+        const user = yield user_model_1.default.findOne({ username });
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+            return;
+        }
+        res.status(200).json(user);
+    }
+    catch (error) {
+        res.status(500).json({ message: "Error getting user", error });
+    }
+});
+// logout
+const logout = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    res.clearCookie("jwt", {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production", // Asegúrate de que esté en true en producción
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+        maxAge: 0, // Establecer maxAge en 0 asegura que la cookie se elimine inmediatamente
+    });
+    res.status(200).json({ message: "Logout successful" });
+});
+// check login
+const checkLogin = (req, res) => {
+    if (req.user) {
+        res.status(200).json({ isLoggedIn: true });
+    }
+    else {
+        res.status(401).json({ isLoggedIn: false });
+    }
+};
+exports.default = {
+    register,
+    login,
+    userProfile,
+    logout,
+    checkLogin,
+};
